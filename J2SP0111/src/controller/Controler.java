e79f98c9d4cccf0e6a47054639919bdf70e304c2
PhucNood <phucnvhd772000@gmail.com>
2021-11-09 20:55:33 +0700

    true update

--- J2SP0111/src/controller/Controler.java
+++ J2SP0111/src/controller/Controler.java
@@ -24,7 +24,7 @@ public class Controler {
     private View view;
     private boolean incal = false;
     private boolean reset = false;
-  
+
     private BigDecimal curentNumber;
     private BigDecimal firstNum = new BigDecimal("0");
     private BigDecimal secondNum;
@@ -36,10 +36,9 @@ public class Controler {
         this.text = view.getTxtDisplay();
 
         ControlAction();
-       
-         
+
     }
-    
+
     private void ControlAction() {
         // Press Number action:
         view.getBtn0().addActionListener((ae) -> { // use lamda expression for funtional interface ActionListener
@@ -102,7 +101,7 @@ public class Controler {
         view.getBtnBack().addActionListener((ae) -> {
             PressBack();
         });
-      
+
         //press dot action
         view.getBtnDot().addActionListener((ae) -> {
             pressDot();
@@ -128,6 +127,30 @@ public class Controler {
         });
 
     }
+    
+     private void lockIferror(){  // if trigger error lock the all callulation
+        if(text.getText().equalsIgnoreCase("ERROR")){
+            view.getBtnPlus().setEnabled(false);
+            view.getBtnSub().setEnabled(false);
+            view.getBtnSqrt().setEnabled(false);
+            view.getBtnPorM().setEnabled(false);
+            view.getBtnDiv().setEnabled(false);
+            view.getBtnDot().setEnabled(false);
+            view.getBtnInvert().setEnabled(false);
+            view.getBtnPercent().setEnabled(false);
+            view.getBtnMul().setEnabled(false);
+        }else{
+            view.getBtnPlus().setEnabled(true);
+            view.getBtnSub().setEnabled(true);
+            view.getBtnSqrt().setEnabled(true);
+            view.getBtnPorM().setEnabled(true);
+            view.getBtnDiv().setEnabled(true);
+            view.getBtnDot().setEnabled(true);
+            view.getBtnInvert().setEnabled(true);
+            view.getBtnPercent().setEnabled(true);
+            view.getBtnMul().setEnabled(true);
+        }
+    }
 
     private BigDecimal getNumber() {
         curentNumber = new BigDecimal(text.getText());
@@ -147,8 +170,9 @@ public class Controler {
             reset = true;
             return;
         }
-
+  
         text.setText(curentNumber.toPlainString());
+        lockIferror();
 
     }
 
@@ -160,16 +184,17 @@ public class Controler {
                 }
                 firstNum = getNumber(); // take the number before press operator 
             } else { // after press oper
+                lockIferror();
                 secondNum = getNumber();
                 switch (operator) {
                     case "+":// Bigdecimal not have overload contructor for Double but Double better represent than primitive type double
-                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue()+secondNum.doubleValue()).toString()); 
+                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue() + secondNum.doubleValue()).toString());
                         break;
                     case "-":
-                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue()-secondNum.doubleValue()).toString());
+                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue() - secondNum.doubleValue()).toString());
                         break;
                     case "*":
-                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue()*secondNum.doubleValue()).toString());
+                        firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue() * secondNum.doubleValue()).toString()).setScale(5, RoundingMode.HALF_UP);
                         break;
                     case "/":
                         if (secondNum.doubleValue() == 0) {
@@ -177,7 +202,7 @@ public class Controler {
                             reset = true; // error reset
                             return;
                         } else {
-                           firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue()/secondNum.doubleValue()).toString());
+                            firstNum = new BigDecimal(Double.valueOf(firstNum.doubleValue() / secondNum.doubleValue()).toString());
                             break;
                         }
 
@@ -193,14 +218,16 @@ public class Controler {
     }
 
     private void equal() {
+        
         if (text.getText().equalsIgnoreCase("ERROR")) {
-            text.setText(firstNum.toPlainString()); // back to first number
+            text.setText("0"); // reset 
+            reset=true;
         } else {
             caculate();  // just excute on calculate
             operator = null;
         }
+        lockIferror();
     }
-   
 
     private void PressBack() {
         StringBuilder builder = new StringBuilder(text.getText());
@@ -208,7 +235,7 @@ public class Controler {
             text.setText("0");
             reset = true;
         }
-        if (text.getText().startsWith("-")) {  
+        if (text.getText().startsWith("-")) {
             if (text.getText().length() > 2) {  // if text length than 2 dele
                 text.setText(builder.deleteCharAt(text.getText().length() - 1).toString()); // delete last number digit
             } else {
@@ -223,7 +250,7 @@ public class Controler {
                 reset = true;
             }
         }
-
+        lockIferror();
         incal = false;
 
     }
@@ -233,9 +260,11 @@ public class Controler {
         operator = null;
         incal = false;
         reset = true;
+        lockIferror();
     }
 
     private void pressDot() {
+        
         if (incal == true || reset == true) { // if in the caculator or reset clear text
             text.setText("0");
             incal = false;
@@ -245,50 +274,69 @@ public class Controler {
         if (text.getText().contains(".") == false) {
             text.setText(text.getText() + ".");
         }
+        lockIferror();
 
     }
 
-    private void pressNegate() {       
-        text.setText(getNumber().negate().toString());
-      incal = false;// make it to be first number maybe
+    private void pressNegate() {
+            lockIferror();
+            text.setText(getNumber().negate().toString());
+            incal = false;// make it to be first number maybe
+        
+        
 
     }
 
-    private void pressInvert() {     
-        curentNumber = getNumber(); // take number in text
-        if (curentNumber.doubleValue() == 0) {
+    private void pressInvert() {
+
+        // take number in text
+        
+        if (text.getText().equalsIgnoreCase("0")) {
             text.setText("ERROR");
+            reset = true;
         } else {
-            String invert_value = Double.valueOf(1/curentNumber.doubleValue()).toString();
-            text.setText(new BigDecimal(invert_value).stripTrailingZeros().toPlainString() );
-           
-            incal = false; // make it source to calculator
-            secondNum = new BigDecimal(invert_value);
             
-        }
+                curentNumber = getNumber();
+                String invert_value = Double.valueOf(1 / curentNumber.doubleValue()).toString();
+                text.setText(new BigDecimal(invert_value).stripTrailingZeros().toPlainString());
+                incal = false; // make it source to calculator
+                secondNum = new BigDecimal(invert_value);
+           
+
+        }       
         reset = true; // if you press any number get new number
+        lockIferror();
 
     }
 
     private void pressPercent() {
-        curentNumber = getNumber();
-        curentNumber = curentNumber.divide(new BigDecimal(100));
-        text.setText(curentNumber.stripTrailingZeros().toPlainString());
-        incal = false;
-        reset = true;
+            lockIferror();
+            curentNumber = getNumber();
+            curentNumber = new BigDecimal(Double.valueOf(curentNumber.doubleValue()/100).toString());
+            text.setText(curentNumber.stripTrailingZeros().toPlainString());
+            incal = false;
+            reset = true;       
+         
+
     }
 
     private void pressSqrt() {
+       
         if (text.getText().startsWith("-")) { // sqrt can not negative
             text.setText("ERROR");
             reset = true;
         } else {
-            curentNumber = getNumber();
-            double sqrt = Math.sqrt(curentNumber.doubleValue()); // Use Math librari for square root
-            text.setText( new BigDecimal( Double.valueOf(sqrt).toString()).stripTrailingZeros().toPlainString() ); 
-            incal = false;
-            reset = true;
+            
+                curentNumber = getNumber();
+                double sqrt = Math.sqrt(curentNumber.doubleValue()); // Use Math librari for square root
+                text.setText(new BigDecimal(Double.valueOf(sqrt).toString()).stripTrailingZeros().toPlainString());
+                incal = false;
+                reset = true;
+            
+
         }
+         lockIferror();
+        
     }
 
 }
